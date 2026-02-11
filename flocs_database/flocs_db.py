7785848514842faa2218d30ad899bed2646cfada
Frits Sweijen <tikk3r@users.noreply.github.com>
2026-02-11 18:57:49 +0000

    Expand DB creation

--- flocs_database/flocs_db.py
+++ flocs_database/flocs_db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from cyclopts import Parameter
-from typing import Annotated
+from typing import Annotated, Iterable
 import cyclopts
 import subprocess
 
@@ -17,19 +17,26 @@ def create(
     table_name: Annotated[
         str, Parameter(help="Directory where MSes are located.")
     ] = "processing_flocs",
+    pipelines: Annotated[Iterable[str], Parameter(help="")] = ["linc"],
 ):
-    cmd = [
-        "sqlite3",
-        dbname,
-        f"create table {table_name}(source_name text, sas_id_calibrator1 text, sas_id_calibrator2 text, sas_id_calibrator_final text, sas_id_target text primary key, status_calibrator1 smallint, status_calibrator2 smallint, status_target smallint);",
-    ]
+    pipelines = list(map(str.lower, pipelines))
+    dbstr = f"create table {table_name}(source_name text default NULL"
+
+    if "linc" in pipelines:
+        dbstr += ", sas_id_calibrator1 text default NULL, sas_id_calibrator2 text default NULL, sas_id_calibrator_final text default NULL, sas_id_target text primary key default NULL, status_calibrator1 smallint default 0, status_calibrator2 smallint default 0, status_target smallint default 0"
+    dbstr += ");"
+
+    cmd = ["sqlite3", dbname, dbstr]
+    print(f"Creating table via: {" ".join(cmd)}")
 
     return_code = subprocess.run(cmd)
     if not return_code:
         raise RuntimeError(f"Failed to create table {table_name} in database {dbname}.")
 
+
 def main():
     app()
+
 
 if __name__ == "__main__":
     main()
